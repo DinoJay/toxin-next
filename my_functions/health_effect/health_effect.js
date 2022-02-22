489b04ace5c1a4fb250b5f19b0c2533a9e6198db
DinoJay <jott88@googlemail.com>
2022-02-22 13:19:55 +0100

    y

--- my_functions/health_effect/health_effect.js
+++ my_functions/health_effect/health_effect.js
@@ -133,21 +133,28 @@
 // 		}	
 // 	`;
 
-exports.handler = async function (event, context) {
-    console.log('context', context);
-    console.log('event', event);
-    // fetch(constructQuery('repeated-toxicity', sparqlQuery)).then((res) => res.json()).then(res => {
-    //     // console.log('res', res)
+const fetch = require('node-fetch')
 
-    //     return {
-    //         statusCode: 200,
-    //         body: JSON.stringify(res),
-    //     };
+const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts'
 
-    // })
+exports.handler = async (event, context) => {
+    let response
+    try {
+        response = await fetch(API_ENDPOINT)
+        // handle response
+    } catch (err) {
+        return {
+            statusCode: err.statusCode || 500,
+            body: JSON.stringify({
+                error: err.message
+            })
+        }
+    }
+
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "test" }),
-    };
-
+        body: JSON.stringify({
+            data: response
+        })
+    }
 }
